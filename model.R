0b76cd84a708130afe2cd7abd233ff4b8eabe96f
Darren McEwan <63265943+darrenmcewan@users.noreply.github.com>
2021-01-21 14:52:05 -0700

    Added new variable, improved readability
    I noticed that during the weekdays, 7,8am and 5-8pm were popular times, but during the weekend, 10am-4pm were popular times. I made a new variable using this.

--- model.R
+++ model.R
@@ -2,8 +2,8 @@ library(tidyverse)
 library(lubridate)
 library(MLmetrics)
 library(caret)
-library(xgboost)
-library(lime)
+
+
 
 #Import and clean data
 train <- read.csv("train.csv")
@@ -20,17 +20,20 @@ ggplot(data=train, aes(x=datetime, y=count, color=season))+geom_point()
 ggplot(data=train, aes(x=datetime, y=count, color=month(datetime)))+geom_point()
 plot_missing(complete)
 plot_correlation(data = complete, type = "continuous", cor_args = "pairwise.complete.obs")
+ggplot(data=train, aes(x=hour, y=count, color=day))+geom_point()
+
 
 
 complete <- rbind(train,test)
 complete$datetime1 <- as.POSIXct(complete$datetime, tz="EST", "%Y-%m-%d %H:%M:%S")
 complete$date  = date(complete$datetime1)
 complete$day  <- wday(complete$datetime1, label=TRUE)
-complete$hour <-  as.factor(hour(complete$datetime1))
-complete$rush_hour <- ifelse(complete$hour >= 7 & complete$hour <= 10 & complete$workingday == 1| complete$hour >= 4 & complete$hour <= 6 & complete$workingday == 1, TRUE, FALSE)
+complete$hour <-  hour(complete$datetime1)
+complete$rush_hour <- ifelse((complete$hour == 7 | complete$hour <= 8 & complete$workingday == 1)| (complete$hour >= 17 & complete$hour <= 20 & complete$workingday == 1), TRUE, FALSE)
 complete$month <- as.factor(month(complete$datetime1))
 complete$year <- as.factor(year(complete$datetime1))
-
+complete$wkendRide <- ifelse((complete$hour >= 10 & complete$hour <= 16) & complete$workingday == 0, TRUE, FALSE)
+complete$hour <- as.factor(complete$hour)
 complete <- complete %>% select(-date, -datetime1)
 
 #Make sure all variables are factors that need to be
@@ -42,6 +45,10 @@ complete$weather<- as.factor(complete$weather)
 
 #Highly correlated with temp, so we can drop it
 complete <- complete %>% select(-atemp)
+
+
+
+
 
 #Change to log because the metric for the competition is rmsle and also the distribution of counts is skewed.
 complete$count = log1p(complete$count)
@@ -101,4 +108,4 @@ preds.frame <- data.frame(datetime = test$datetime, count = preds)
 #In the case of any negative predictions, we will set it at 0
 preds.frame$count<- ifelse(preds.frame$count < 0,0,preds.frame$count)
 
-write.csv(preds.frame, "xgboost_log.csv", row.names = FALSE)
+write.csv(preds.frame, "xgboost_log1.csv", row.names = FALSE)
