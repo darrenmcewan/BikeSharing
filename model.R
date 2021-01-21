4098bcbf7bae0b75465019a303ef177d2691716d
Darren McEwan <63265943+darrenmcewan@users.noreply.github.com>
2021-01-21 13:34:30 -0700

    Update model.R

--- model.R
+++ model.R
@@ -26,10 +26,10 @@ complete <- rbind(train,test)
 complete$datetime1 <- as.POSIXct(complete$datetime, tz="EST", "%Y-%m-%d %H:%M:%S")
 complete$date  = date(complete$datetime1)
 complete$day  <- wday(complete$datetime1, label=TRUE)
-complete$hour <-  hour(complete$datetime1)
+complete$hour <-  as.factor(hour(complete$datetime1))
 complete$rush_hour <- ifelse(complete$hour >= 7 & complete$hour <= 10 & complete$workingday == 1| complete$hour >= 4 & complete$hour <= 6 & complete$workingday == 1, TRUE, FALSE)
-complete$month <- month(complete$datetime1)
-complete$year <- year(complete$datetime1)
+complete$month <- as.factor(month(complete$datetime1))
+complete$year <- as.factor(year(complete$datetime1))
 
 complete <- complete %>% select(-date, -datetime1)
 
@@ -38,11 +38,9 @@ complete$season<- as.factor(complete$season)
 complete$holiday<- as.factor(complete$holiday)
 complete$workingday<- as.factor(complete$workingday)
 complete$weather<- as.factor(complete$weather)
-complete$hour<- as.factor(complete$hour)
-complete$month <- as.factor(complete$month)
-complete$year <- as.factor(complete$year)
 
-#Highly correlated with temp, so we can leave it
+
+#Highly correlated with temp, so we can drop it
 complete <- complete %>% select(-atemp)
 
 #Change to log because the metric for the competition is rmsle and also the distribution of counts is skewed.
@@ -92,12 +90,15 @@ gbm$bestTune
 #     200        15   0.1     0              0.8                1         1
 
 
+#Gather predictions
 preds <- predict(gbm, newdata = test)
 
 #Change preds back from log
 preds <- expm1(preds)
 
 preds.frame <- data.frame(datetime = test$datetime, count = preds)
+
+#In the case of any negative predictions, we will set it at 0
 preds.frame$count<- ifelse(preds.frame$count < 0,0,preds.frame$count)
 
 write.csv(preds.frame, "xgboost_log.csv", row.names = FALSE)
